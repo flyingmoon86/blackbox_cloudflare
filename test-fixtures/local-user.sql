7d0b96a1c376965df2ede412452bb4931ad6412b
Yang QiMing <moonflying56@gmail.com>
2026-09-09 09:38:50 +0800

    本地测试 fixture 增加 admin 管理员账号

--- test-fixtures/local-user.sql
+++ test-fixtures/local-user.sql
@@ -9,3 +9,15 @@ INSERT OR REPLACE INTO user (
   'admin',
   'active'
 );
+
+INSERT OR REPLACE INTO user (
+  id, username, password_hash, email, auth_version, role, status
+) VALUES (
+  900002,
+  'admin',
+  'scrypt:32768:8:1$11208eee03d6a0b379d3ac551b0e7b28$cfe9a9942582e951d4157e6359e6828ac06451d03166c2ab97c9c86a6076f36c4507f34311d09b8eb5df775d7453e5b64ebe37319727642f6212159c2b2d3138',
+  NULL,
+  0,
+  'admin',
+  'active'
+);
